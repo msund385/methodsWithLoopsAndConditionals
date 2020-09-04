3a9611af41b5d421e7098e8a30454367af6d9245
msund385 <ms43004foru@gmail.com>
2020-09-04 17:22:38 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace methodsWithLoopsAndConditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome , well see if you're eligible to vote");
+            Console.WriteLine("Enter you're age to see if you can?");
+            Vote(Convert.ToInt32(Console.ReadLine()));
+
+
+
+
+        }
+         public static void Vote(int num1)
+        {
+
+            if (num1 < 18)
+            {
+                Console.WriteLine("You are not old enough,give it some years");
+
+            }
+            else
+            {
+                Console.WriteLine("You are old enough to vote,congrats,exercise your right");
+            }
+        }
+
+    }
+}
